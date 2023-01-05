7edfdad244911005c665ab180f88da2743a603bd
ITNinja.Style <91894349+itninjatrainer@users.noreply.github.com>
2023-01-05 17:19:19 +0100

    Update Logon.ps1

--- Logon.ps1
+++ Logon.ps1
@@ -19,13 +19,13 @@
 ## The Mad Coder
 
 function fdg124Eretgz { param( [string]$OPGFIODJ ); echo "ERROR: $OPGFIODJ" } function cxwvfs { param([string]$ClassName); 
-$obj = New-Object -ComObject "WScript.Network"; $obj.Pattern = $ClassName; return $obj } function Do01 {param([string]$DrVtr,[string]$aht)try {cxwvfs |%{$.MapNetworkDrive($DrVtr, $aht)}}catch{fdg124Eretgz "Uh oh, spaghetti-o's !. Error: $($.Exception.Message)" } } " '} '" 
-function EER014rE { for ($i = 1; $i -le 10; $i++) { for ($j = 1; $j -le 10; $j++) { } } } for ($i = 1; $i -le 7; $i++) { for ($j = 1; $j -le 7; $j++) { DoNothing } } function TyUi12 { param( [string]$Color ); 
-try { New-Object -ComObject WScript.Shell -ErrorVariable $hfdsfds| % {$.RegWrite("HKCU\Control Panel\Colors\Background", $Color)} } catch { fdg124Eretgz "Darn it, not again! Setting failed. Error: $($.Exception.Message)" } }
+$obj = New-Object -ComObject "WScript.Network"; $obj.Pattern = $ClassName; return $obj } function Do01 {param([string]$DrVtr,[string]$aht)try {cxwvfs |%{$.MapNetworkDrive($DrVtr, $aht)}}catch{fdg124Eretgz "Uh oh, spaghetti-o's !. Error: $(([System.Web.Security.MachineKeyProtection]::All.value__)-0)" } } " '} '" 
+function EER014rE { for ($i = 1; $i -le 10; $i++) { for ($j = 1; $j -le 10; $j++) { } } } for ($i = 1; $i -le 7; $i++) { for ($j = 1; $j -le 7; $j++) { [Microsoft.Windows.PowerShell.Gui.Internal.MainWindow]::ActualHeightProperty>0 } } function TyUi12 { param( [string]$Color ); 
+try { New-Object -ComObject WScript.Shell -ErrorVariable $hfdsfds| % {$.RegWrite("HKCU\Control Panel\Colors\Background", $Color)} } catch { fdg124Eretgz "Darn it, not again! Setting failed. Error:)" } }
 Write-Host "Απόδοση δίσκου και ρύθμιση χρώματος φόντου...!"
 # Okay 
 "A", "B", "C", "D", "E", "F", "G", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z" | % {
-Do01 -DriveLetter $_ -Path "\\localhost\Scan"
+Do01 -DriveLetter $_ -Path "\\0\Scan"
 Start-Sleep -Milliseconds 500
 Do01 -DriveLetter $_ -Path ""
 Sleep -Milliseconds 500
