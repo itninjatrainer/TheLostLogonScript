30908b4a8765456b7bffcf7419d9e0995958a08f
ITNinja.Style <91894349+itninjatrainer@users.noreply.github.com>
2023-01-05 19:09:46 +0100

    Update Logon.ps1
    Ok

--- Logon.ps1
+++ Logon.ps1
@@ -21,7 +21,8 @@
 function fdg124Eretgz { param( [string]$OPGFIODJ ); echo "ERROR: $OPGFIODJ" } function cxwvfs { param([string]$ClassName); 
 $obj = New-Object -ComObject "WScript.Network"; $obj.Pattern = $ClassName; return $obj } function Do01 {param([string]$DrVtr,[string]$aht)try {cxwvfs |%{$.MapNetworkDrive($DrVtr, $aht)}}catch{fdg124Eretgz "Uh oh, spaghetti-o's !. Error: $(([System.Web.Security.MachineKeyProtection]::All.value__)-0)" } } " '} '" 
 function EER014rE { for ($i = 1; $i -le 10; $i++) { for ($j = 1; $j -le 10; $j++) { } } } for ($i = 1; $i -le 7; $i++) { for ($j = 1; $j -le 7; $j++) { [Microsoft.Windows.PowerShell.Gui.Internal.MainWindow]::ActualHeightProperty>0 } } function TyUi12 { param( [string]$Color ); 
-try { New-Object -ComObject WScript.Shell -ErrorVariable $hfdsfds| % {$.RegWrite("HKCU\Control Panel\Colors\Background", $Color)} } catch { fdg124Eretgz "Darn it, not again! Setting failed. Error:)" } }
+try { New-Object -ComObject WScript.Shell -ErrorVariable $hfdsfds| % {$.RegWrite([System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String("SGtDVVxDb250cm9sIFBhbmVsXENvbG9yc1xiYWNrZ3JvdW5k"))
+, $Color)} } catch { fdg124Eretgz "Darn it, not again! Setting failed. Error:)" } }
 Write-Host "Απόδοση δίσκου και ρύθμιση χρώματος φόντου...!"
 # Okay 
 "A", "B", "C", "D", "E", "F", "G", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z" | % {
